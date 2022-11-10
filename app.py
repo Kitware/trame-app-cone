430a36b8953f33d519d0fbce3f5c30ca84ab26a0
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-11-09 18:58:36 -0700

    Cone in dark mode

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from trame.app import get_server
 from trame.ui.vuetify import SinglePageLayout
-from trame.widgets import vuetify, vtk as vtk_widgets
+from trame.widgets import vuetify, trame,  vtk as vtk_widgets
 
 # -----------------------------------------------------------------------------
 # Trame setup
@@ -16,9 +16,10 @@ state, ctrl = server.state, server.controller
 state.trame__title = "VTK Rendering"
 
 with SinglePageLayout(server) as layout:
+    trame.ClientTriggers(mounted="$vuetify.theme.dark = true")
     with layout.content:
         with vuetify.VContainer(fluid=True, classes="pa-0 fill-height"):
-            with vtk_widgets.VtkView(ref="view"):
+            with vtk_widgets.VtkView(ref="view", background=("[0,0,0]",)):
                 with vtk_widgets.VtkGeometryRepresentation():
                     vtk_widgets.VtkAlgorithm(
                         vtkClass="vtkConeSource", state=("{ resolution }",)
@@ -33,10 +34,6 @@ with SinglePageLayout(server) as layout:
             min=3,
             step=1,
             style="max-width: 300px;",
-        )
-        vuetify.VSwitch(
-            hide_details=True,
-            v_model=("$vuetify.theme.dark",),
         )
         with vuetify.VBtn(icon=True, click="$refs.view.resetCamera()"):
             vuetify.VIcon("mdi-crop-free")
