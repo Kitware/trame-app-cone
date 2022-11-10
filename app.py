75ab64798ea2b3815c579242cf1dde0e056a72d4
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2022-11-10 10:42:37 -0700

    use sphere instead of cone

--- app.py
+++ app.py
@@ -19,24 +19,28 @@ with SinglePageLayout(server) as layout:
     with layout.content:
         with vuetify.VContainer(fluid=True, classes="pa-0 fill-height"):
             with vtk_widgets.VtkView(ref="view"):
-                with vtk_widgets.VtkGeometryRepresentation():
+                with vtk_widgets.VtkGeometryRepresentation(property=("{ edgeVisibility: true }",)):
                     vtk_widgets.VtkAlgorithm(
-                        vtk_class="vtkConeSource", state=("{ resolution }",)
+                        vtk_class="vtkSphereSource", state=("{ phiResolution, thetaResolution }",)
                     )
 
     with layout.toolbar:
         vuetify.VSpacer()
         vuetify.VSlider(
             hide_details=True,
-            v_model=("resolution", 6),
+            v_model=("phiResolution", 6),
             max=60,
             min=3,
             step=1,
             style="max-width: 300px;",
         )
-        vuetify.VSwitch(
+        vuetify.VSlider(
             hide_details=True,
-            v_model=("$vuetify.theme.dark",),
+            v_model=("thetaResolution", 6),
+            max=60,
+            min=3,
+            step=1,
+            style="max-width: 300px;",
         )
         with vuetify.VBtn(icon=True, click="$refs.view.resetCamera()"):
             vuetify.VIcon("mdi-crop-free")
